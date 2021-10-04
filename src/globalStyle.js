bdd737af869dbfbebd43e7ece29ee7dbd9dad729
thiago-porto25 <thiago_porto25@hotmail.com>
2021-10-04 10:25:39 -0300

    change global style

--- src/globalStyle.js
+++ src/globalStyle.js
@@ -1,16 +1,18 @@
 import { createGlobalStyle } from 'styled-components'
 
 export const globalStyle = createGlobalStyle`
+
+  :root{
+    --light: #f5f8fa;
+    --dark: #13171a;
+    --lightGrey: #e1e8ed;
+    --grey: #aab8c2;
+  }
+
   html, body {
     font-family: 'Roboto', Helvetica, Arial, sans-serif;
-    background-color: #000000;
-    color: #333333;
+    color: var(--light);
     font-size: 16px;
     margin: 0;
-
-    $light: #f5f8fa;
-    $dark: #13171a;
-    $lightGrey: #e1e8ed;
-    $grey: #aab8c2;
   }
   `
