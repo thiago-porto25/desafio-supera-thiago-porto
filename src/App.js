7c25d6be4de1894f25f41d611278420aea80a891
thiago-porto25 <thiago_porto25@hotmail.com>
2021-10-04 09:52:53 -0300

    set up routing, add lazy loading

--- src/App.js
+++ src/App.js
@@ -1,5 +1,29 @@
+import { lazy, Suspense } from 'react'
+import { Route, Switch } from 'react-router-dom'
+import * as ROUTES from '@src/constants/routes'
+
+const Home = lazy(() => import('@pages/home'))
+const Shop = lazy(() => import('@pages/shop'))
+const Product = lazy(() => import('@pages/product'))
+
 function App() {
-  return <>test</>
+  return (
+    <Suspense fallback={<div>loading...</div>}>
+      <Switch>
+        <Route exact path={ROUTES.HOME}>
+          <Home />
+        </Route>
+
+        <Route path={ROUTES.SHOP}>
+          <Shop />
+        </Route>
+
+        <Route path={ROUTES.PRODUCT}>
+          <Product />
+        </Route>
+      </Switch>
+    </Suspense>
+  )
 }
 
 export default App
