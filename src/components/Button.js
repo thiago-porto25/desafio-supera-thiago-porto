a1de1bb1778157a7521ebe2a57f4587d7469cc09
thiago-porto25 <thiago_porto25@hotmail.com>
2021-10-06 14:16:46 -0300

    add style and functionality to button

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,7 +1,26 @@
 import styled from 'styled-components'
 
-const Btn = styled.button``
+const Btn = styled.button`
+  width: 100%;
+  height: 100%;
+  font-weight: bold;
+  transition: 150ms ease;
+  cursor: pointer;
+  font-size: 20px;
 
-export default function Button() {
-  return <Btn></Btn>
+  &.white {
+    color: var(--dark);
+    background-color: var(--light);
+    border: 1px solid var(--light);
+
+    &:hover {
+      color: var(--light);
+      background-color: var(--dark);
+      border: 1px solid var(--dark);
+    }
+  }
+`
+
+export default function Button({ children, type }) {
+  return <Btn className={type}>{children}</Btn>
 }
