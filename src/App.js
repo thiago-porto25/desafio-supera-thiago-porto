c8b0223def35abe3a708c5a815466ad1e9c98702
thiago-porto25 <thiago_porto25@hotmail.com>
2021-10-06 12:55:16 -0300

    add background to pages

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { lazy, Suspense } from 'react'
-import { Route, Switch } from 'react-router-dom'
+import { Route, Switch, useLocation } from 'react-router-dom'
 
 import { Header } from './components'
 
@@ -11,26 +11,30 @@ const Shop = lazy(() => import('./pages/shop'))
 const Product = lazy(() => import('./pages/product'))
 
 function App() {
+  const { pathname } = useLocation()
+
   return (
-    <CartContextProvider>
-      <Header />
+    <div className={`${pathname === '/' ? 'with-background' : ''}`}>
+      <CartContextProvider>
+        <Header />
 
-      <Suspense fallback={<div>loading...</div>}>
-        <Switch>
-          <Route exact path={ROUTES.HOME}>
-            <Home />
-          </Route>
+        <Suspense fallback={<div>loading...</div>}>
+          <Switch>
+            <Route exact path={ROUTES.HOME}>
+              <Home />
+            </Route>
 
-          <Route path={ROUTES.SHOP}>
-            <Shop />
-          </Route>
+            <Route path={ROUTES.SHOP}>
+              <Shop />
+            </Route>
 
-          <Route path={ROUTES.PRODUCT}>
-            <Product />
-          </Route>
-        </Switch>
-      </Suspense>
-    </CartContextProvider>
+            <Route path={ROUTES.PRODUCT}>
+              <Product />
+            </Route>
+          </Switch>
+        </Suspense>
+      </CartContextProvider>
+    </div>
   )
 }
 
--- src/globalStyle.js
+++ src/globalStyle.js
@@ -5,14 +5,25 @@ export const globalStyle = createGlobalStyle`
   :root{
     --light: #f5f8fa;
     --dark: #13171a;
-    --lightGrey: #e1e8ed;
+    --lightGrey: #faf2f2;
     --grey: #aab8c2;
   }
 
   html, body {
     font-family: 'Roboto', Helvetica, Arial, sans-serif;
+    background-color: var(--lightGrey);
     color: var(--dark);
     font-size: 16px;
     margin: 0;
+  }
+
+  .with-background {
+    height: 100vh;
+    width: 100%;
+    background-image: url('/images/fundo.jpg');
+    background-position: center;
+    background-size: cover;
+    background-repeat: no-repeat;
+    color: var(--light);
   }
   `
