e11a0529b6b21daebf88520229f3c783bea46fed
thiago-porto25 <thiago_porto25@hotmail.com>
2021-10-06 18:10:29 -0300

    finish styling of shop item and the shop template

--- src/components/ShopItem.js
+++ src/components/ShopItem.js
@@ -1,7 +1,73 @@
 import styled from 'styled-components'
+import { Link } from 'react-router-dom'
+import { RiHeartsLine } from 'react-icons/ri'
 
-const Container = styled.li``
+const Container = styled.li`
+  background-color: white;
+  color: var(--dark);
+  cursor: pointer;
+  display: flex;
+  justify-content: space-evenly;
+  flex-direction: column;
+  box-shadow: 0px 0px 5px #00000022;
+  height: 100%;
+  transition: 200ms ease-in-out;
 
-export default function ShopItem() {
-  return <Container></Container>
+  &:hover {
+    box-shadow: 0px 0px 5px #00000088;
+  }
+
+  .item-image {
+    padding: 2rem;
+    box-sizing: border-box;
+  }
+
+  p {
+    box-sizing: border-box;
+    padding: 0.5rem 1rem;
+    margin: 0;
+  }
+
+  .item-title {
+    font-weight: bold;
+    font-size: 20px;
+  }
+
+  .item-score {
+    color: #555;
+    display: flex;
+    align-items: center;
+    gap: 0.5rem;
+
+    svg {
+      color: red;
+      font-size: 20px;
+    }
+  }
+
+  .item-price {
+    margin-bottom: 1rem;
+    font-size: 18px;
+  }
+`
+
+export default function ShopItem({ item }) {
+  return (
+    <Link to={`product/${item.id}`}>
+      <Container>
+        <img
+          className="item-image"
+          src={`/images/assets/${item.image}`}
+          alt={item.name}
+        />
+        <p className="item-title">{item.name}</p>
+        <p className="item-score">
+          <RiHeartsLine /> {item.score}
+        </p>
+        <p className="item-price">
+          R${(Math.round(item.price * 100) / 100).toFixed(2)}
+        </p>
+      </Container>
+    </Link>
+  )
 }
--- src/templates/ShopTemplate.js
+++ src/templates/ShopTemplate.js
@@ -1,7 +1,46 @@
+import { useEffect, useState } from 'react'
 import styled from 'styled-components'
+import { Filter, ShopItem } from '../components'
+import data from '../data/products.json'
 
-const Container = styled.main``
+const Container = styled.main`
+  display: grid;
+  grid-template-columns: 3fr 9fr;
+  min-height: calc(100vh - 7rem);
+
+  .shop-items-list {
+    margin: 0;
+    display: grid;
+    grid-template-columns: repeat(auto-fit, minmax(250px, 1fr));
+    gap: 4rem;
+    list-style-type: none;
+    box-sizing: border-box;
+    padding: 1rem 4rem 4rem 4rem;
+
+    a {
+      text-decoration: none;
+    }
+  }
+`
 
 export default function ShopTemplate() {
-  return <Container></Container>
+  const [shopOrder, setShopOrder] = useState([])
+
+  useEffect(() => {
+    setShopOrder(data)
+  }, [])
+
+  return (
+    <Container>
+      <div className="filter-container">
+        <Filter shopOrder={shopOrder} setShopOrder={setShopOrder} />
+      </div>
+
+      <ul className="shop-items-list">
+        {shopOrder.map((item) => (
+          <ShopItem key={item.id} item={item} />
+        ))}
+      </ul>
+    </Container>
+  )
 }
