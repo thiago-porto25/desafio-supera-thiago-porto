3ee2863d853860f6dedaecc54b35340c2bea840b
thiago-porto25 <thiago_porto25@hotmail.com>
2021-10-06 16:22:31 -0300

    create filter and its logic

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -1,7 +1,64 @@
 import styled from 'styled-components'
 
-const Container = styled.div``
+const Container = styled.div`
+  box-sizing: border-box;
+  padding: 4rem 0 4rem 4rem;
 
-export default function Filter() {
-  return <Container></Container>
+  .filter-title {
+    font-size: 25px;
+    font-weight: bold;
+  }
+
+  ul {
+    list-style-type: none;
+    padding: 0;
+
+    li {
+      display: flex;
+      align-items: center;
+      gap: 0.5rem;
+      margin-top: 1rem;
+      margin-bottom: 1rem;
+    }
+  }
+`
+
+export default function Filter({ shopOrder, setShopOrder }) {
+  const order = (type) => {
+    if (type === 'price') {
+      const sortedCart = shopOrder.sort((a, b) => a.price - b.price)
+      setShopOrder([...sortedCart])
+    }
+
+    if (type === 'score') {
+      const sortedCart = shopOrder.sort((a, b) => a.score - b.score)
+      setShopOrder([...sortedCart.reverse()])
+    }
+
+    if (type === 'alpha') {
+      const sortedCart = shopOrder.sort((a, b) => a.name.localeCompare(b.name))
+      setShopOrder([...sortedCart])
+    }
+  }
+
+  return (
+    <Container>
+      <p className="filter-title">Ordenar por:</p>
+
+      <ul>
+        <li>
+          <input onChange={() => order('price')} type="radio" name="order" />
+          Preço
+        </li>
+        <li>
+          <input onChange={() => order('score')} type="radio" name="order" />
+          Popularidade
+        </li>
+        <li>
+          <input onChange={() => order('alpha')} type="radio" name="order" />
+          Ordem alfabética
+        </li>
+      </ul>
+    </Container>
+  )
 }
