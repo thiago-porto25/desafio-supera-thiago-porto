62c57d2b74ed7e2269eedaaaf62c8f900d955e89
thiago-porto25 <thiago_porto25@hotmail.com>
2021-10-06 18:19:31 -0300

    add responsiveness to shop page

--- src/templates/ShopTemplate.js
+++ src/templates/ShopTemplate.js
@@ -8,14 +8,23 @@ const Container = styled.main`
   grid-template-columns: 3fr 9fr;
   min-height: calc(100vh - 7rem);
 
+  @media (max-width: 950px) {
+    display: flex;
+    flex-direction: column;
+  }
+
   .shop-items-list {
     margin: 0;
     display: grid;
-    grid-template-columns: repeat(auto-fit, minmax(250px, 1fr));
+    grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));
     gap: 4rem;
     list-style-type: none;
     box-sizing: border-box;
     padding: 1rem 4rem 4rem 4rem;
+
+    @media (max-width: 300px) {
+      padding: 1rem 1rem 4rem 1rem;
+    }
 
     a {
       text-decoration: none;
